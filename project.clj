b65f306b8c44feb2297201e5b95a7177e4f9fff4
Andrew Garman <agarman@carelogistics.com>
2014-09-19 15:06:51 -0400

    fixed error handling bug
    removed unnecessary macro

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject somni "0.2.0"
+(defproject somni "0.2.1"
   :description "An opinionated yet lightweight services routing library"
   :url "https://github.com/CareLogistics/somni"
   :license {:name "Eclipse Public License 1.0"
