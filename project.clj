5cd94b6ec647bd0bbefcb668d8d2bfd72c96cea0
Andrew Garman <agarman@carelogistics.com>
2015-03-05 08:55:44 -0500

    new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject somni "0.3.1"
+(defproject somni "0.3.2"
   :description "An opinionated yet lightweight services routing library"
   :url "https://github.com/CareLogistics/somni"
   :license {:name "Eclipse Public License 1.0"
