b31cbfb4dae3b94c1080104ba0c2691bf1062e65
Andrew Garman <agarman@carelogistics.com>
2015-06-16 15:53:29 -0400

    minimal dependency injection for functions

--- src/somni/injectors.clj
+++ src/somni/injectors.clj
@@ -1,24 +1,43 @@
 (ns somni.injectors
   (:require [somni.misc :refer [unthunk]]))
 
-(defn var->args
-  [f]
-  (:arglists (meta f)))
-
-(defn invoke-with-map
+(defn partial-from-map
   [f m]
   (let [m (reduce (fn [a [k v]] (assoc a (symbol (name k)) v)) {} m)
-        {:as m :keys [arglists]} (meta f)
+        {:as f-meta :keys [arglists]} (meta f)
         matched (first (filter #(every? m %) arglists))
         params (seq (map m matched))]
-    (if params
-      (eval `(~f ~@params))
-      (with-meta (fn [m2] (invoke-with-map f (merge m m2))) m))))
 
-(defn wrap-deps
-  [handler deps]
-  (fn [{:as request :keys [body headers]}]
-    ((assoc (unthunk deps)
-       :request request
-       :body    body
-       :headers headers))))
+    (with-meta
+      (if params
+        (fn []   (eval `(~f ~@params)))
+        (fn [m2] (partial-from-map f (merge m m2))))
+      f-meta)))
+
+(def ^:dynamic *dep-generators* {:body     :body
+                                 :headers  :headers
+                                 :identity :identity
+                                 :params   :params
+                                 :request  identity})
+
+(defn- request->deps
+  [request]
+  (reduce (fn [a [k f]] (if-some [v (f request)]
+                         (assoc a k v)
+                         a))
+          nil
+          *dep-generators*))
+
+(defn wrap-deps*
+  ([handler deps dep-gen]
+   (with-redefs [*dep-generators* dep-gen]
+     (fn [request]
+       ((partial-from-map handler
+                          (merge (unthunk deps)
+                                 (request->deps request)))))))
+
+  ([handler deps] (wrap-deps* handler deps *dep-generators*)))
+
+(defmacro wrap-deps
+  ([handler deps dep-gen] `(wrap-deps* #'~handler ~deps ~dep-gen))
+  ([handler deps]         `(wrap-deps* #'~handler ~deps)))
--- test/somni/injectors_test.clj
+++ test/somni/injectors_test.clj
@@ -4,9 +4,23 @@
 
 (defn a [foo bar baz quux] (+ foo bar baz quux))
 
-(deftest test-invoke-with-map
+(deftest partial-from-map-test
   (let [f (atom #'a)]
-    (is (ifn? (swap! f invoke-with-map {:foo 1})))
+    (is (ifn? (swap! f partial-from-map {:foo 1})))
     (is (ifn? (swap! f apply {:bar 2})))
     (is (ifn? (swap! f apply {'baz 3})))
-    (is (= 10 (swap! f apply {"quux" 4})))))
+    (is (ifn? (swap! f apply {"quux" 4})))
+    (is (= 10 (@f)))))
+
+(deftest request->deps-test
+  (let [request->deps #'somni.injectors/request->deps]
+    (is (= (request->deps {:body 123, :headers {}, :identity {}})
+           {:request {:body 123, :headers {}, :identity {}}
+            :body 123,
+            :headers {},
+            :identity {}}))))
+
+(def deps {:foo 1 'bar 2 "quux" 3 :baz 4})
+(deftest wrap-deps-test
+  (let [a-with-deps (wrap-deps* #'a deps)]
+    (is (= 10 (a-with-deps {})))))
