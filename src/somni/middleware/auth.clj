62381084cf396538c44123607b2579fba12873f5
Andrew Garman <agarman@carelogistics.com>
2015-06-17 13:00:14 -0400

    more refactoring, unit testing

--- src/somni/middleware/auth.clj
+++ src/somni/middleware/auth.clj
@@ -1,49 +1,83 @@
-(ns somni.middleware.auth)
+(ns somni.middleware.auth
+  (:require [somni.http.errors :refer [not-authenticated
+                                       access-denied]]))
 
-(defn wrap-access-control
-  "
-  Wraps the handler in an ACL check.  ACLs are specified on http
-  methods.  If acl-fn is nil, the user's role will be extracted
-  from [:identity :role] of the request.
+(defmulti  request->identity
+  "Returns identity that should be associated with the request.
+  If there is no identity, returns nil.
 
-  Roles are compared by name; :abc, 'abc & \"abc\" are the same role.
+  *anonymous* returns an example identity for anonymous requests."
+  (fn [auth-provider request] auth-provider))
 
-  Returns 403 if ACL check fails.
-  "
-  [handler acls & [acl-fn policy]]
+(defmethod request->identity :default [& _] nil)
 
-  {:pre [handler]}
+(def ^:dynamic *anonymous* (fn [request] {:user :anon, :roles nil}))
 
-  (let [role-lookup (or acl-fn #(get-in % [:identity :role]))
-        acls (into {} (for [[k v] acls] [k (set (map name v))]))]
-
-    (cond
-     (seq acls) (fn [{:as request :keys [request-method]}]
-                  (let [macl (get acls request-method)
-                        role (role-lookup request)
-                        role (and role (name role))]
-
-                    (if (or (nil? macl)
-                            (macl role))
-                      (handler request)
-                      (access-denied request))))
-
-     (= policy :deny) access-denied
-     :else            handler)))
+(defmethod request->identity :none [_ request] (*anonymous* request))
 
 (defn wrap-authentication
-  "
-  Extremely simple authentication.  If the security function returns
-  a value, that value is associated with the request as the identity
-  and the handler will be invoked.
+  "Authentication of request using request->identity multimethod"
+  [handler auth-provider]
 
-  Returns a 401 if sec-fn returns nil.
-  "
-  [handler sec-fn]
-
-  {:pre [handler sec-fn]}
+  {:pre [(ifn? handler)
+         (keyword? auth-provider)]}
 
   (fn [request]
-    (if-some [id (sec-fn request)]
-      (handler (assoc request :identity id))
+    (if-some [id (request->identity auth-provider request)]
+      (-> request (assoc :identity id)
+          handler (assoc :identity id))
       (not-authenticated request))))
+
+(defmulti  request->roles
+  "Returns set of roles associated with a request.  If there is no user
+  or the user has no roles, request->roles should return nil."
+  (fn [role-provider request] role-provider))
+
+(defmethod request->roles :default
+  [_ request]
+  (get-in request [:identity :roles]))
+
+(defn- acls*
+  [acls]
+  (into {} (for [[k v] acls] [k (set (map name v))])))
+
+(defn- access-allowed?
+  "Tests if there's an intersection"
+  [allowed-roles users-roles]
+  (some (comp allowed-roles name) users-roles))
+
+(defn- wrap-access-control*
+  [handler acls role-provider]
+
+  {:pre [(seq acls)]}
+
+  (prn acls)
+
+  (fn [{:as request :keys [request-method]}]
+
+    (let [allowed-roles (acls request-method)
+          users-roles   (set (request->roles role-provider request))]
+
+      (if (access-allowed? allowed-roles users-roles)
+        (handler request)
+        (access-denied request)))))
+
+(defn wrap-access-control
+  "ACL based authorization middleware that uses request->roles multimethod
+  to extract a users roles from request.
+
+  acls is a map from operation to valid roles, e.g.:
+    {:get [roles that are valid], :put [other roles]}
+
+  When no acls are provided for the handler, returns to access-denied for all
+  requests."
+  ([handler acls role-provider]
+
+   {:pre [(ifn? handler)]}
+
+   (let [acls (acls* acls)]
+     (if (seq acls)
+       (wrap-access-control* handler acls role-provider)
+       access-denied)))
+
+  ([handler acls] (wrap-access-control handler acls :default)))
---
+++ test/somni/middleware/auth_test.clj
@@ -0,0 +1,38 @@
+(ns somni.middleware.auth-test
+  (:require [somni.middleware.auth :refer :all]
+            [clojure.test :refer :all]))
+
+(def ^:private test-user {:user 'test, :roles '#{tester}})
+
+(defmethod request->identity :test [& _] test-user)
+
+(defn- h [{:as request :keys [identity]}]
+  (if (= identity test-user)
+    {:status 200}
+    (throw (ex-info "Missing user identity" {}))))
+
+(deftest wrap-authentication-test
+  (is (= test-user (:identity ((wrap-authentication h :test) {})))
+      "Authenticated requests retain identity through request & response")
+
+  (is (= 401 (:status ((wrap-authentication h :ssl) {})))
+      "Unknown auth-types produce not-authenticated")
+
+  (is (= (*anonymous* {})
+         (:identity ((wrap-authentication identity :none) {})))
+      ":none authentication produces *anonymous* identity"))
+
+(def test-req {:request-method :get
+               :identity test-user})
+
+(def test-acl {:get [:tester]})
+
+(deftest wrap-access-control-test
+  (is (= 200 (:status ((wrap-access-control h test-acl) test-req)))
+      "Authorization allows access when user has role")
+
+  (is (= 403 (:status ((wrap-access-control h test-acl) {:request-method :get})))
+      "Access denied when identity is unknown")
+
+  (is (= 403 (:status ((wrap-access-control h nil) test-req)))
+      "Access denied when no roles assigned to resource"))
