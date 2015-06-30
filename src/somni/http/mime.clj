fb792578ac470ad5882d1292aa2c2f2de7f3da0f
Andrew Garman <agarman@carelogistics.com>
2015-06-30 07:38:46 -0400

    switched mime parser to output liberator style representation

--- src/somni/http/mime.clj
+++ src/somni/http/mime.clj
@@ -3,33 +3,16 @@
             [somni.misc :refer [desc]])
   (:import (java.nio.charset Charset)))
 
-(def ^:private accept-rexp #"\s*,\s*")
+(defn- glob? [x] (= "*" x))
 
-(defn- split-accept
-  [a]
-  (str/split (or a "") accept-rexp))
+(defn- count-glob [m] (count (filter (comp glob? val) m)))
 
-(def ^:private mime-params-rexp #"\s*\/\s*|\s*\;\s*|\s*\=\s*")
-
-(defn- split-mime
-  [a]
-  (str/split (or a "") mime-params-rexp))
-
-(def ^:private mime-suffix-rexp #"\+")
-
-(defn- parse-mime-type [[group sub & params]]
-  (let [[media suffix] (str/split (or sub "") mime-suffix-rexp)]
-    [(merge {:group group,
-             :media media
-             :mime (str group "/" media (when suffix (str "+" suffix)))}
-            (when suffix {:suffix suffix}))
-     params]))
-
-(defn- parse-mime-params
-  [[mime & [params]]]
-  (reduce (fn [a [k v]] (assoc a (keyword k) v))
-          (assoc mime :q "1.0")
-          (partition 2 params)))
+(defn- sort-mimes
+  [xs]
+  (->> xs
+       (sort-by count-glob)
+       (sort-by count desc)
+       (sort-by :q    desc)))
 
 (def ^:private charset-for-name
   (memoize
@@ -42,26 +25,38 @@
   [{:as mime :keys [charset]}]
   (assoc mime :charset (charset-for-name charset)))
 
+(defn- parse-mime-params
+  [[mime & [params]]]
+  (reduce (fn [a [k v]] (assoc a (keyword k) v))
+          (assoc mime :q "1.0")
+          (partition 2 params)))
+
+(def ^:private mime-suffix-rexp #"\+")
+
+(defn- parse-media-type [[group sub & params]]
+  (let [[media suffix] (str/split (or sub "") mime-suffix-rexp)]
+    [(merge {:group group,
+             :media media
+             :media-type (str group "/" media (when suffix (str "+" suffix)))}
+            (when suffix {:suffix suffix}))
+     params]))
+
+(def ^:private mime-params-rexp #"\s*\/\s*|\s*\;\s*|\s*\=\s*")
+
+(defn- split-mime [a] (str/split (or a "") mime-params-rexp))
+
 (def parse-mime
-  (memoize                              ; adds ~9x speed up
+  (memoize
    (comp parse-charset
          parse-mime-params
-         parse-mime-type
+         parse-media-type
          split-mime)))
 
-(defn- glob? [x] (= "*" x))
+(def ^:private accept-rexp #"\s*,\s*")
 
-(defn- count-glob [m] (count (filter (comp glob? val) m)))
-
-(defn- sort-mimes
-  [xs]
-  (->> xs
-       (sort-by count-glob)
-       (sort-by count desc)
-       (sort-by :q    desc)))
+(defn- split-accept [a] (str/split (or a "") accept-rexp))
 
 (defn parse-accept [a]
-  ;;; NOTE: accept have no fixed ordering, so pointless to memoize
   (let [[h & t :as xs] (when (seq a) (map parse-mime (split-accept a)))]
     (cond
      t     (sort-mimes xs)
