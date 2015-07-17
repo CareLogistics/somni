b48c21834b9b27380ee10ec815d4385b3fe7a397
Andrew Garman <agarman@carelogistics.com>
2015-07-17 09:29:31 -0400

    add license headers

--- src/somni/middleware/validation.clj
+++ src/somni/middleware/validation.clj
@@ -1,6 +1,17 @@
+;;; Copyright (c) Care Logistics, inc. All rights reserved.
+;;; The use and distribution terms for this software are covered by the
+;;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;; which can be found in the file epl-v10.html at the root of this
+;;; distribution.
+;;; By using this software in any fashion, you are agreeing to be bound by
+;;; the terms of this license.
+;;; You must not remove this notice, or any other, from this software.
+
 (ns somni.middleware.validation
   (:require [schema.core :as s]
             [somni.http.errors :refer [malformed-request]]))
+
+;; TODO: herbert v. prismatic
 
 (defn wrap-request-validation
   "
