e3a0b2f00c05e86e62d082ebde64ac03f46e5dba
Andrew Garman <agarman@carelogistics.com>
2015-07-17 08:28:09 -0400

    clean-up extract* fn

--- src/somni/middleware/extractions.clj
+++ src/somni/middleware/extractions.clj
@@ -4,24 +4,25 @@
             [somni.misc :refer [uri->path]]))
 
 (defn- extract*
-  [obj [a & [b & z :as t] :as xpath]]
+  [obj xpath]
   (cond
     (empty? xpath) [obj]
 
     (nil? obj) []
 
     (map? obj) (for [[k v] obj
-                     :when (= (name k) a)
-                     x (extract* v t)]
+                     :when (= (name k) (first xpath))
+                     x (extract* v (next xpath))]
                  x)
 
     (coll? obj) (for [i obj
                       :let [m (if (map? i) i (bean i))]
                       [k v] m
-                      :when (= (name k) a)
+                      :when (= (name k) (first xpath))
+                      :let [b (second xpath)]
                       x (if (and b (= (str v) b))
-                          (extract* m z)
-                          (extract* v t))]
+                          (extract* m (drop 2 xpath))
+                          (extract* v (next xpath)))]
                   x)
 
     :else (extract* (bean obj) xpath)))
